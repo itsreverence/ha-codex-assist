58c5f749411f704ee08e58420af6997349a55b9b
itsreverence <github.shakily561@passmail.net>
2026-08-29 23:44:42 -0500

    Use multiline selector for system prompt

--- tests/ha_fakes.py
+++ tests/ha_fakes.py
@@ -141,6 +141,14 @@ def install_homeassistant_fakes(monkeypatch):
     class SelectSelector:
         config: SelectSelectorConfig
 
+    @dataclass
+    class TextSelectorConfig:
+        multiline: bool = False
+
+    @dataclass
+    class TextSelector:
+        config: TextSelectorConfig
+
     class Section:
         def __init__(self, schema, options=None):
             self.schema = schema
@@ -218,6 +226,8 @@ def install_homeassistant_fakes(monkeypatch):
     selector.SelectSelector = SelectSelector
     selector.SelectSelectorConfig = SelectSelectorConfig
     selector.SelectSelectorMode = SelectSelectorMode
+    selector.TextSelector = TextSelector
+    selector.TextSelectorConfig = TextSelectorConfig
     util_json.json_loads = __import__("json").loads
     util.slugify = lambda value: value.lower().replace(" ", "_")
     voluptuous_openapi.convert = lambda schema, custom_serializer=None: getattr(
